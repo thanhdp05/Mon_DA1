a112a0ebb6d16d1f16284aa68c129a8f6839918d
thanhdp05 <Thanhphuocdinh05@gmail.com>
2024-07-09 14:41:50 +0700

    thanh/entityClass

---
+++ DA1/src/main/java/com/ntdk/entity/loaiSP.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ntdk.entity;
+
+/**
+ *
+ * @author ASUS
+ */
+public class loaiSP {
+    int maLoai ;
+    String tenLoai ;
+
+    public loaiSP() {
+    }
+
+    public loaiSP(int maLoai, String tenLoai) {
+        this.maLoai = maLoai;
+        this.tenLoai = tenLoai;
+    }
+
+    public int getMaLoai() {
+        return maLoai;
+    }
+
+    public void setMaLoai(int maLoai) {
+        this.maLoai = maLoai;
+    }
+
+    public String getTenLoai() {
+        return tenLoai;
+    }
+
+    public void setTenLoai(String tenLoai) {
+        this.tenLoai = tenLoai;
+    }
+    
+}
